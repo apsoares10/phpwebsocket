f740c51d4b0f0bef1ba27b10f08021a6ca1102e5
apsoares10 <110748368+apsoares10@users.noreply.github.com>
2022-08-10 21:09:08 -0300

    Add files via upload

---
+++ server/websocket_server.php
@@ -0,0 +1,58 @@
+<?php
+set_time_limit(0);
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+require_once '../vendor/autoload.php';
+
+class Chat implements MessageComponentInterface {
+	protected $clients;
+	// protected $users;
+
+	public function __construct() {
+		$this->clients = new \SplObjectStorage;
+	}
+
+	public function onOpen(ConnectionInterface $conn) {
+		$this->clients->attach($conn);
+		// $this->users[$conn->resourceId] = $conn;
+	}
+
+	public function onClose(ConnectionInterface $conn) {
+		$this->clients->detach($conn);
+		// unset($this->users[$conn->resourceId]);
+	}
+
+	public function onMessage(ConnectionInterface $from,  $data) {
+		$from_id = $from->resourceId;
+		$data = json_decode($data);
+		$type = $data->type;
+		switch ($type) {
+			case 'chat':
+				$user_id = $data->user_id;
+				$chat_msg = $data->chat_msg;
+				$response_from = "<span style='color:#999'><b>".$user_id.":</b> ".$chat_msg."</span><br><br>";
+				$response_to = "<b>".$user_id."</b>: ".$chat_msg."<br><br>";
+				// Output
+				$from->send(json_encode([
+					"type"=>$type,"msg"=>$response_from
+				]));
+				foreach($this->clients as $client) {
+					if($from!=$client) {
+						$client->send(json_encode([
+							"type"=>$type,"msg"=>$response_to
+						]));
+					}
+				}
+				break;
+		}
+	}
+
+	public function onError(ConnectionInterface $conn, \Exception $e) {
+		$conn->close();
+	}
+}
+$server = new Ratchet\App('localhost', 8080);
+$server->route('/', new Chat, ['*']);
+$server->run();
+?>
